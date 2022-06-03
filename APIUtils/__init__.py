3b0ccfd2a7be95cde068afc5d4bbf7580bb540ff
Mythxcal <75327936+Mythxcal@users.noreply.github.com>
2022-06-03 14:39:07 +0100

    Update __init__.py

--- APIUtils/__init__.py
+++ APIUtils/__init__.py
@@ -5,52 +5,6 @@ from discord.ext import commands
 import random
 
 
-
-
-class Generator:
-    def user_agent(headers=None):
-        if headers == None:
-            f = open(r"C:\Users\aisha\AppData\Local\Programs\Python\Python38\Lib\site-packages\APIUtils\Data\user_agents.md", 'r').readlines()
-            return random.choice(f).replace('\n', '')
-        
-            
-        f = open(r"C:\Users\aisha\AppData\Local\Programs\Python\Python38\Lib\site-packages\APIUtils\Data\user_agents.md", 'r').readlines()
-        headers["user_agent"] = random.choice(f).replace('\n', '')
-        return headers
-
-        
-        return random.choice(f).replace('\n', '')
-    
-    def proxy(formatted=True):
-        if formatted:
-            f = open(r"C:\Users\aisha\AppData\Local\Programs\Python\Python38\Lib\site-packages\APIUtils\Data\proxies.md", 'r').readlines()
-            return {"proxies": random.choice(f).replace('\n', '')}
-        if formatted == False:
-            f = open(r"C:\Users\aisha\AppData\Local\Programs\Python\Python38\Lib\site-packages\APIUtils\Data\proxies.md", 'r').readlines()
-            return random.choice(f).replace('\n', '')
-        
-class ROBLOX:
-    def __init__(self, cookie):
-        self.cookie = cookie
-    
-    def get_token(self):
-        noTokenHeaders = {
-            "Cookie":".ROBLOSECURITY={}".format(self.cookie),
-        }
-        url = "https://auth.roblox.com/"
-        abc = requests.post(url, headers=noTokenHeaders)
-        return abc.headers['x-csrf-token']
-
-    def headers(self):
-        headers = {
-            "Cookie": f".ROBLOSECURITY={self.cookie}",
-            'x-csrf-token': ROBLOX.get_token(self)
-        }
-        return headers
-    
-    
-
-
 class Discord:
     def __init__(self, token):
         self.token = token
@@ -65,8 +19,3 @@ class Discord:
             "authorization": self.token
         }
         return header
-    
-        
-        
-
-
